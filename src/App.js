dec1fab178b16364553718af295a342926c138dd
Yahiaabdo36 <45404044+yahiaelpronc@users.noreply.github.com>
2022-07-24 05:32:02 +0200

    Linked Footer Component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@ import './App.css';
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {BrowserRouter,Route,Switch,Link} from 'react-router-dom'
-import UserData from './pages/UserData'
-import AppFunctionComponent from './pages/AppFunctionComponent'
-import AppClassComponent from './pages/AppClassComponent'
-import AddUserForm from './pages/AddUserForm'
 import NotFound from './pages/NotFound'
-import ListUsers from './pages/ListUsers'
 import NavbarComponent from './Components/NavbarComponent'
 import FooterComponent from './Components/FooterComponent';
 import Home from './pages/HomePage';
@@ -22,11 +17,10 @@ function App() {
           {/* Pages */} 
           
           <Route path={"/"} exact component={Home}/>
-          <Route path={"/UserData"} exact component={UserData}/>
-          <Route path={"/ListUsers"} exact component={ListUsers}/>
           {/* Test Pages */}
           <Route path={"*"} exact component={NotFound}/>
           </Switch>
+          <FooterComponent/>
         </BrowserRouter>
       </div>
     );
