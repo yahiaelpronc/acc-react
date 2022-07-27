a4af8e6cf71a388856f87eea24a66de928751151
Eman Mahmoud <emanmahmood706@gmail.com>
2022-07-27 12:13:15 +0200

    operations

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {BrowserRouter,Route,Switch,Link} from 'react-router-dom'
+import ScheduledOperation from './pages/ScheduledOperation';
 import NotFound from './pages/NotFound'
 import NavbarComponent from './Components/NavbarComponent'
 import Myform from './Components/Myform';
@@ -33,7 +34,6 @@ import AddUserForm from './pages/AddUserForm';
 import Register from './pages/Register';
 import VetRegister from './pages/VetRegister';
 import UserRegister from './pages/UserRegister';
-import ScheduledOperation from './pages/ScheduledOperation';
 import RequestSur from './pages/RequestSur';
 import ServicesRequest from './pages3/ServicesRequest';
 function App() {
