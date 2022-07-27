1d32fcd66aa2c40be774c4f33b772bec1051d36e
Yahiaabdo36 <45404044+yahiaelpronc@users.noreply.github.com>
2022-07-27 18:21:36 +0200

    a

--- src/pages/UserRegister.jsx
+++ src/pages/UserRegister.jsx
@@ -16,7 +16,7 @@ function UserRegister() {
             "Gharbia", "Giza", "Ismailia", "Kafr El Sheikh", "Luxor", "Matruh", "Minya", "Monufia", "New Valley",
             "North Sinai", "Port Said", "Qalyubia", "Qena", "Red Sea", "Sharqia", "Sohag", "South Sinai", "Suez"]
     )
-    const [image, setImage] = useState(null)
+    const [image, setImage] = useState("")
     const [userData, setUserData] = useState({
         username: "",
         password: "",
@@ -35,58 +35,239 @@ function UserRegister() {
         firstnameErr: "",
         lastnameErr: "",
         emailErr: "",
-        phoneErr: "",
         passwordErr: "",
-        confirmpassErr: "",
         cityErr: "",
+        phoneErr: "",
+        confirmpassErr: "",
         b_dateErr: "",
         addressErr: "",
         facebook_linkErr: "",
     })
-
+    const [errdataB, seterrdataB] = useState({
+        usernameErrB: false,
+        firstnameErrB: false,
+        lastnameErrB: false,
+        emailErrB: false,
+        passwordErrB: false,
+        phoneErrB: false,
+        confirmpassErrB: false,
+        b_dateErrB: false,
+        addressErrB: false,
+        facebook_linkErrB: false,
+    })
+    function errors(e) {
+        if (errdataB.usernameErrB
+            && errdataB.firstnameErrB
+            && errdataB.lastnameErrB
+            && errdataB.emailErrB
+            && errdataB.passwordErrB
+            && errdataB.phoneErrB
+            && errdataB.confirmpassErrB
+            && errdataB.b_dateErrB
+        ) {
+            return false
+        }
+        return true
+    }
+    function isInTheFuture(date) {
+        let today = new Date();
+        // 👇️ OPTIONAL!
+        // This line sets the time of the current date to the
+        // last millisecond, so the comparison returns `true` only if
+        // date is at least tomorrow
+        today.setHours(23, 59, 59, 998);
+        console.log(date)
+        return date > today;
+    }
 
     function changeData(e) {
         if (e.target.name === "username") {
-            setUserData({
-                ...userData,
-                username: e.target.value
-            })
+            if (e.target.value.length < 3 | e.target.value.length > 30) {
+                seterrdata({
+                    ...errdata,
+                    usernameErr: "This Field Has To Be 3 to 30 Characters Long"
+                })
+                seterrdataB({
+                    ...errdataB,
+                    usernameErrB: false
+                })
+            } else {
+                seterrdataB({
+                    ...errdataB,
+                    usernameErrB: true
+                })
+                seterrdata({
+                    ...errdata,
+                    usernameErr: true
+                })
+                setUserData({
+                    ...userData,
+                    username: e.target.value
+                })
+                console.log(errdataB)
+            }
         }
         else if (e.target.name === "password") {
-            setUserData({
-                ...userData,
-                password: e.target.value
-            })
+            if (e.target.value.length < 3 | e.target.value.length > 30) {
+                seterrdata({
+                    ...errdata,
+                    passwordErr: "This Field Has To Be 3 to 30 Characters Long"
+                })
+                seterrdataB({
+                    ...errdataB,
+                    passwordErrB: false
+                })
+            } else {
+                seterrdata({
+                    ...errdata,
+                    passwordErr: ""
+                })
+                seterrdataB({
+                    ...errdataB,
+                    passwordErrB: true
+                })
+                setUserData({
+                    ...userData,
+                    password: e.target.value
+                })
+            }
         }
         else if (e.target.name === "firstname") {
-            setUserData({
-                ...userData,
-                firstname: e.target.value
-            })
+            if (e.target.value.length < 3 | e.target.value.length > 30) {
+                seterrdata({
+                    ...errdata,
+                    firstnameErr: "This Field Has To Be 3 to 30 Characters Long"
+                })
+                seterrdataB({
+                    ...errdataB,
+                    firstnameErrB: false
+                })
+            } else {
+                seterrdata({
+                    ...errdata,
+                    firstnameErr: ""
+                })
+                seterrdataB({
+                    ...errdataB,
+                    firstnameErrB: true
+                })
+                setUserData({
+                    ...userData,
+                    firstname: e.target.value
+                })
+            }
         }
         else if (e.target.name === "lastname") {
-            setUserData({
-                ...userData,
-                lastname: e.target.value
-            })
+            if (e.target.value.length < 3 | e.target.value.length > 30) {
+                seterrdata({
+                    ...errdata,
+                    lastnameErr: "This Field Has To Be 3 to 30 Characters Long"
+                })
+                seterrdataB({
+                    ...errdataB,
+                    lastnameErrB: false
+                })
+            } else {
+                seterrdata({
+                    ...errdata,
+                    lastnameErr: ""
+                })
+                seterrdataB({
+                    ...errdataB,
+                    lastnameErrB: true
+                })
+                setUserData({
+                    ...userData,
+                    lastname: e.target.value
+                })
+            }
         }
         else if (e.target.name === "email") {
-            setUserData({
-                ...userData,
-                email: e.target.value
-            })
+            if (e.target.value.length < 3 | e.target.value.length > 30) {
+                seterrdata({
+                    ...errdata,
+                    emailErr: "This Field Has To Be 3 to 30 Characters Long"
+                })
+                seterrdataB({
+                    ...errdataB,
+                    emailErrB: false
+                })
+            } else {
+                seterrdata({
+                    ...errdata,
+                    emailErr: ""
+                })
+                seterrdataB({
+                    ...errdataB,
+                    emailErrB: true
+                })
+                setUserData({
+                    ...userData,
+                    email: e.target.value
+                })
+            }
         }
         else if (e.target.name === "phone") {
-            setUserData({
-                ...userData,
-                phone: e.target.value
-            })
+            if (e.target.value.length < 3 | e.target.value.length > 30) {
+                seterrdata({
+                    ...errdata,
+                    phoneErr: "This Field Has To Be 3 to 30 Characters Long"
+                })
+                seterrdataB({
+                    ...errdataB,
+                    phoneErrB: false
+                })
+            } else {
+                seterrdata({
+                    ...errdata,
+                    phoneErr: ""
+                })
+                seterrdataB({
+                    ...errdataB,
+                    phoneErrB: true
+                })
+                setUserData({
+                    ...userData,
+                    phone: e.target.value
+                })
+            }
         }
         else if (e.target.name === "confirmpass") {
-            setUserData({
-                ...userData,
-                confirmpass: e.target.value
-            })
+            if (e.target.value.length < 3 | e.target.value.length > 30) {
+                if (e.target.value !== userData.password) {
+                    seterrdata({
+                        ...errdata,
+                        confirmpassErr: "This Field Has To Be 3 to 30 Characters Long"
+                    })
+                    seterrdataB({
+                        ...errdataB,
+                        confirmpassErrB: false
+                    })
+                }
+            } else if (e.target.value !== userData.password) {
+                seterrdata({
+                    ...errdata,
+                    confirmpassErr: "Passwords Don't Match"
+                })
+                seterrdataB({
+                    ...errdataB,
+                    confirmpassErrB: false
+                })
+            }
+            else {
+                seterrdata({
+                    ...errdata,
+                    confirmpassErr: ""
+                })
+                seterrdataB({
+                    ...errdataB,
+                    confirmpassErrB: true
+                })
+                setUserData({
+                    ...userData,
+                    confirmpass: e.target.value
+                })
+            }
         }
         else if (e.target.name === "city") {
             setUserData({
@@ -95,22 +276,79 @@ function UserRegister() {
             })
         }
         else if (e.target.name === "b_date") {
-            setUserData({
-                ...userData,
-                b_date: String(e.target.value).replace("/", "-")
-            })
+            if (isInTheFuture(new Date(String(e.target.value)))) {
+                seterrdata({
+                    ...errdata,
+                    b_dateErr: "Please Choose A Correct Birth Date"
+                })
+                seterrdataB({
+                    ...errdataB,
+                    b_dateErrB: false
+                })
+            } else {
+                seterrdata({
+                    ...errdata,
+                    b_dateErr: ""
+                })
+                seterrdataB({
+                    ...errdataB,
+                    b_dateErrB: true
+                })
+                setUserData({
+                    ...userData,
+                    b_date: e.target.value
+                })
+            }
         }
         else if (e.target.name === "address") {
-            setUserData({
-                ...userData,
-                address: e.target.value
-            })
+            if (e.target.value.length < 3 | e.target.value.length > 30) {
+                seterrdata({
+                    ...errdata,
+                    addressErr: "This Field Has To Be 3 to 30 Characters Long"
+                })
+                seterrdataB({
+                    ...errdataB,
+                    addressErrB: false
+                })
+            } else {
+                seterrdata({
+                    ...errdata,
+                    addressErr: ""
+                })
+                seterrdataB({
+                    ...errdataB,
+                    addressErrB: true
+                })
+                setUserData({
+                    ...userData,
+                    address: e.target.value
+                })
+            }
         }
         else {
-            setUserData({
-                ...userData,
-                facebook_link: e.target.value
-            })
+            if (e.target.value.length < 3 | e.target.value.length > 30) {
+                seterrdata({
+                    ...errdata,
+                    facebook_linkErr: "This Field Has To Be 3 to 30 Characters Long"
+                })
+                seterrdataB({
+                    ...errdataB,
+                    facebook_linkErrB: false
+                })
+            } else {
+                seterrdata({
+                    ...errdata,
+                    facebook_linkErr: ""
+                })
+                seterrdataB({
+                    ...errdataB,
+                    facebook_linkErrB: true
+                })
+                setUserData({
+                    ...userData,
+                    facebook_link: e.target.value
+                })
+            }
         }
     }
     const RegisterUser = async (e) => {
@@ -126,12 +364,21 @@ function UserRegister() {
         formField.append("mobile", userData.phone)
         formField.append("b_date", userData.b_date)
         formField.append("face_link", userData.facebook_link)
+        // if(image === ""){
         formField.append("profile_pic", image)
+        // }
+        // else{
+        //     formField.append("profile_pic", image)
+        // }
         await axios({
             method: 'post',
             url: 'http://127.0.0.1:8000/api/insertUser/',
             data: formField
-        }).then((res) => history.push("/login"))
+        }).then((res) => {
+            // if (res.data)
+            console.log(res.data.length)
+            // history.push("/login")
+        })
             .catch((err) => console.log(err))
     }
     return <>
@@ -143,60 +390,64 @@ function UserRegister() {
                 <div className="user-details">
                     <div className="input-box">
                         <span className="details">Username</span>
-                        <input type="text" className="form-control" placeholder="Username" aria-label="Username"
+                        <input required type="text" className="form-control" placeholder="Username" aria-label="Username"
                             aria-describedby="basic-addon1" onChange={(e) => changeData(e)} name="username" />
+                        <p className="text-danger" style={{ fontSize: "13px" }}>{errdata.usernameErr}</p>
                     </div>
                     <div className="input-box">
                         <span className="details">First Name</span>
                         <input type="text" name="firstname" placeholder="Enter Your fisrt Name" id="name" onChange={(e) => changeData(e)} />
-                        <p id="nameerr"></p>
+                        <p className="text-danger" style={{ fontSize: "13px" }}>{errdata.firstnameErr}</p>
                     </div>
                     <div className="input-box">
                         <span className="details">last Name</span>
                         <input type="text" name="lastname" placeholder="Enter Your last Name" id="user" onChange={(e) => changeData(e)} />
-                        <p id="usererr"></p>
+                        <p className="text-danger" style={{ fontSize: "13px" }}>{errdata.lastnameErr}</p>
                     </div>
                     <div className="input-box">
                         <span className="details">Email</span>
                         <input type="text" name="email" placeholder="Enter Your Email" id="email" onChange={(e) => changeData(e)} />
-                        <p id="emailerr"></p>
+                        <p className="text-danger" style={{ fontSize: "13px" }}>{errdata.emailErr}</p>
                     </div>
                     <div className="input-box">
                         <span className="details">Phone Number</span>
                         <input type="number" name="phone" placeholder="Enter Your egyption PhoneNumber" id="phone" onChange={(e) => changeData(e)} />
-                        <p id="phoneerr"></p>
+                        <p className="text-danger" style={{ fontSize: "13px" }}>{errdata.phoneErr}</p>
                     </div>
                     <div className="input-box">
                         <span className="details">Password</span>
                         <input type="password" name="password" placeholder="Enter Your Password" id="pass1" onChange={(e) => changeData(e)} />
-                        <p id="pass1err"></p>
+                        <p className="text-danger" style={{ fontSize: "13px" }}>{errdata.passwordErr}</p>
                     </div>
-
                     <div className="input-box">
                         <span className="details">Confirm Password</span>
-                        <input type="password" name="confirmpassword" placeholder="Confirm Your Password" id="pass2" onChange={(e) => changeData(e)} />
-                        <p id="pass2err"></p>
+                        <input type="password" name="confirmpass" placeholder="Confirm Your Password" id="pass2" onChange={(e) => changeData(e)} />
+                        <p className="text-danger" style={{ fontSize: "13px" }}>{errdata.confirmpassErr}</p>
                     </div>
                     <div className="input-box">
                         <span className="details">City</span>
                         <select className="form-select" name="city" aria-label="Default select example" onChange={(e) => changeData(e)}>
                             {governorates.map(gov => {
                                 return (<>
+                                    {gov === "Cairo" &&
+                                        <option selected value={gov}>{gov}</option>
+                                    }
                                     <option value={gov}>{gov}</option>
                                 </>)
                             })}
                         </select>
+                        <p className="text-danger" style={{ fontSize: "13px" }}>{errdata.cityErr}</p>
                     </div>
 
                     <div className="mb-3">
                         <label htmlFor="bdate" className="form-label label1">Birth Date</label>
                         <input type="date" className="form-control" id="bdate" aria-describedby="emailHelp"
                             placeholder=" Birth Date" name="b_date" onChange={(e) => changeData(e)} />
+                        <p className="text-danger" style={{ fontSize: "13px" }}>{errdata.b_dateErr}</p>
                     </div>
                     <div className="my-2">
                         <span className="details">upload a profile pic.</span>
                         <input type="file" placeholder="upload" id="file" onChange={(e) => setImage(e.target.files[0])} />
-                        <p id="fileerr"></p>
                     </div>
                 </div>
 
@@ -215,6 +466,7 @@ function UserRegister() {
                     <label htmlFor="Address" className="form-label label1">Address</label>
                     <input type="text" className="form-control" id="Address" aria-describedby="emailHelp"
                         placeholder="Address" name="address" onChange={(e) => changeData(e)} />
+                    <p className="text-danger" style={{ fontSize: "13px" }}>{errdata.addressErr}</p>
                 </div>
 
 
@@ -222,11 +474,12 @@ function UserRegister() {
                     <label htmlFor="faceLink" className="form-label label1">Facebook Page</label>
                     <input type="text" className="form-control" id="faceLink" aria-describedby="emailHelp"
                         placeholder="http//www.Facebook.com/fn.page" name="facebook" onChange={(e) => changeData(e)} />
+                    <p className="text-danger" style={{ fontSize: "13px" }}>{errdata.facebook_linkErr}</p>
                 </div>
 
 
                 <div className="button">
-                    <input type="submit" value="Register" id="submitbtn" onClick={(e) => RegisterUser(e)} />
+                    <input disabled={errors()} type="submit" value="Register" id="submitbtn" onClick={(e) => RegisterUser(e)} />
                 </div>
 
 
--- src/pages3/SurgeryRequests.jsx
+++ src/pages3/SurgeryRequests.jsx
@@ -28,15 +28,11 @@ function SurgeryRequest() {
 
         <div className='bg-light p-3'>
             <div className='row p-2'>
-
                 <img className='col-3 h-25 w-25 rounded' src={require(`./myimages/schedule1.png`)} />
                 <div className='col-6 '>
                     <center> <h2 className=' text-danger my-4 '>Surgery Requests</h2></center>
-
                     <h4 className='  my-5 '>See Your Latest schedule For Surgeries</h4>
-
                 </div>
-
             </div>
             {Requests.map(req => {
                 return (<>
