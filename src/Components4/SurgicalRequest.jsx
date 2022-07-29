da857328ee85b5a4f47b8db8498dca5509cedaf5
aya-m-98 <106470807+aya-m-98@users.noreply.github.com>
2022-07-29 16:34:28 +0200

    SurgicalRequestFINALLY

---
+++ src/Components4/SurgicalRequest.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './Component4Static/SurgicalRequest.css';
+class SurgicalRequest extends React.Component {
+    constructor(props) {
+        super();
+    }
+    render() {
+        return (
+            <>
+                <div className=" container surContainer">
+                    <div className='MESSAG row p-3 my-3'>
+                        <p className='messagesss fw-bold'></p>
+
+                    </div>
+                    <div className='ffff row'>
+                        <div className='col-6'>
+                            <div className=' card mmCard'>
+                                <ul className="list-group list-group-flush">
+
+                                    <li className="list-group-item">
+                                        <label className="labels" htmlFor=""> Animal Type:</label>
+                                        <span className="span22"></span>
+                                    </li>
+                                    <li className="list-group-item">
+                                        <label className="labels" htmlFor=""> Username:</label>
+                                        <span className="span22"></span>
+                                    </li>
+                                </ul>
+                            </div>
+
+
+                        </div>
+                    </div>
+
+                </div>
+
+
+
+            </>
+        )
+
+    }
+}
+export default SurgicalRequest;
